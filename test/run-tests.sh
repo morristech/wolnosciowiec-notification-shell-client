d926e24ee45268d552857093440cc215636af90f
Black and Red <wesoly.krzysztofa@gmail.com>
2017-03-26 15:42:14 +0200

    Added license, corrected readme.md for github syntax

---
+++ test/run-tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo '{"content": "Lenin is not comparable to any revolutionary figure in history. Revolutionaries have had ideals. Lenin has none. He is a madman, an immolator, wishful of burning, and slaughter, and sacrificing.", "title": "A Kropotkin quote", "group_name": "email"}' | ./bin/notification-send --type MailMessage
